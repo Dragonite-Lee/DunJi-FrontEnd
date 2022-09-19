024b1d51df83e46f5ab7a920289b5463a68cb4c3
dragonite-lee <ross1222@naver.com>
2022-09-20 04:23:12 +0900

    <#10> plus파일 생성

---
+++ components/main/tabBar/Plus.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import useMainRedux from "hooks/useMainRedux";
+import { dispatchTabBarOpenHome, dispatchTabBarOpenSearch, dispatchTabBarOpenChat, dispatchTabBarOpenMypage} from "store/modules/main";
+
+export default function Plus() {
+
+    return (
+        <div>
+            개발중
+        </div>
+    )
+}
