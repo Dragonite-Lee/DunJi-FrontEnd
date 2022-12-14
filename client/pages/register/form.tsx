6202403345e8fbbaaefe137bc74e2dd988ec9120
sumi-0011 <selina2000@naver.com>
2022-12-14 10:24:23 +0900

    refactor: form item add

--- client/pages/register/form.tsx
+++ client/pages/register/form.tsx
@@ -1,4 +1,6 @@
-export default function PolicyForm({}) {
+import FormItem from 'client/pages/register/form-item';
+
+function PolicyForm() {
   return (
     <section className="w-main_width min-w-20 flex items-center flex-col my-40 mx-auto font-sans">
       <h1 className="w-full border-b border-yellow pb-4 text-3xl font-bold">
@@ -7,29 +9,25 @@ export default function PolicyForm({}) {
       <h2 className="text-lg py-8 mr-auto font-bold">
         둥지 서비스 이용을 위해 아래 정보를 입력해 주세요.
       </h2>
+
       <form className="flex flex-col w-full">
-        <label className="text-lg mb-2 mt-4 font-bold">아이디</label>
-        <input
-          className="outline-0 border-blur border h-12 pl-4 focus:border-yellow"
-          placeholder="이메일 주소 입력"
-          type="email"
-        ></input>
-        <label className="text-lg mb-2 mt-4 font-bold">이용자 이름</label>
-        <input
-          className="outline-0 border-blur border h-12 pl-4 focus:border-yellow"
+        <FormItem label="아이디" placeholder="이메일 주소 입력" type="email" />
+        <FormItem
+          label="이용자 이름"
           placeholder="한글 또는 영문"
-        ></input>
-        <label className="text-lg mb-2 mt-4 font-bold">비밀번호</label>
-        <input
-          className="outline-0 border-blur border h-12 pl-4 mb-4 focus:border-yellow"
-          type="password"
+          type="text"
+        />
+        <FormItem
+          label="비밀번호"
           placeholder="8자리 이상 영문, 숫자, 특수문자 포함"
-        ></input>
-        <input
-          className="outline-0 border-blur border h-12 pl-4 focus:border-yellow mb-4"
           type="password"
+        />
+        <FormItem
+          label="비밀번호 확인"
           placeholder="비밀번호 확인"
-        ></input>
+          type="password"
+        />
+
         <button type="submit" className="bg-yellow h-16 font-sans text-2xl">
           확인
         </button>
@@ -37,3 +35,5 @@ export default function PolicyForm({}) {
     </section>
   );
 }
+
+export default PolicyForm;
