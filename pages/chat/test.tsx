ac400bba135f2bc5ac6cc17825070e17d170afe9
sumi-0011 <selina2000@naver.com>
2022-12-28 17:27:36 +0900

    refactor: chat page folder fix

---
+++ pages/chat/test.tsx
@@ -0,0 +1,12 @@
+import TabBar from 'components/main/TabBar';
+
+function Test() {
+  return (
+    <div className="bg-background_beige h-screen sm:w-[375px] sm:m-auto">
+      <TabBar />
+      Chat 개발중
+    </div>
+  );
+}
+
+export default Test;
