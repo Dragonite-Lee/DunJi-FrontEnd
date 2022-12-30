54c3d0cb7edf4296ab2b5f3d7db181479edf0aa2
sumi-0011 <selina2000@naver.com>
2022-12-31 01:06:45 +0900

    refactor: chat test file refactor

--- pages/chat/test.tsx
+++ pages/chat/test.tsx
@@ -1,114 +1,122 @@
-import React, { useEffect, useState } from 'react';
-// import './Chatting.css';
+import { ChangeEvent, KeyboardEvent, useCallback, useState } from 'react';
 import SockJS from 'sockjs-client';
-// import * as StompJs from '@stomp/stompjs';
+
+const SERVER_URL = 'http://127.0.0.1:9999/echo';
+
+interface ChatMessageType {
+  name: string;
+  message: string;
+}
 
 const Chatting = () => {
-  // chatting 토클 상태
-  const [live, setLive] = useState(false);
-  // 메세지 유저 및 내용
-  const [message, setMessage] = useState('');
-  //서버 url
-  const [serverUrl, setServerUrl] = useState('http://127.0.0.1:9999/');
-  // 서버로 부터 받아온 내용
-  const [chat, setChat] = useState([]);
+  const [live, setLive] = useState<boolean>(false);
+  const [message, setMessage] = useState<string>('');
+  const [serverUrl, setServerUrl] = useState<string>(SERVER_URL);
+  const [chat, setChat] = useState<ChatMessageType[]>([]);
+  const [sockjs, setSockjs] = useState<WebSocket | null>();
 
-  const [sockjs, setSockjs] = useState();
-  const [receivedData, setReceivedData] = useState('');
+  const handleReceiveMessage = (e: MessageEvent) => {
+    const newReceiveData = e.data;
+    if (newReceiveData === '') return;
+    setChat((chat) => [...chat, { name: 'Server', message: newReceiveData }]);
+  };
 
-  useEffect(() => {
-    console.log(chat);
-  }, [chat]);
+  const handleSocketConnect = () => {
+    const sock: WebSocket = new SockJS(serverUrl);
 
-  useEffect(() => {
-    if (receivedData === '') return;
-    setChat([...chat, { name: 'Server', message: receivedData }]);
-  }, [receivedData]);
+    sock.onmessage = handleReceiveMessage;
 
-  const onClickConnectBtn = () => {
-    const sock = new SockJS(serverUrl + 'echo');
-    sock.onmessage = function (e) {
-      setReceivedData(e.data);
-      console.log(e.data);
-    };
     setSockjs(sock);
     setChat([...chat, { name: 'testUser', message: '님이 입장하셨습니다.' }]);
     setLive(true);
   };
-  const onClickDisconnectBtn = () => {
+
+  const handleSocketDisConnect = () => {
     setLive(false);
   };
-  const inputMessage = (e) => {
+
+  const inputMessage = (e: ChangeEvent<HTMLInputElement>) => {
     setMessage(e.target.value);
   };
-  const onEnter = (e) => {
+
+  const onEnter = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.keyCode === 13) {
       sendMessage();
     }
   };
+
   const sendMessage = () => {
     if (message === '') return;
+    if (!sockjs) return;
+
     setChat([...chat, { name: 'testUser', message: message }]);
-    console.log(message);
-    console.log(sockjs);
+    // console.log(sockjs);
     sockjs.send(message);
     setMessage('');
   };
-  const setChatServerURL = (e) => {
+
+  const handleChatServerURLChange = (e: ChangeEvent<HTMLInputElement>) => {
     setServerUrl(e.target.value);
   };
-  const renderChat = () => {
-    console.log(chat);
+
+  const renderChat = useCallback(() => {
     return chat.map(({ name, message }, index) => (
-      <div key={index}>
-        <>
-          {name}: <>{message}</>
-        </>
-      </div>
+      <ChatMessage key={index} name={name} message={message} />
     ));
-  };
+  }, [chat]);
+
+  if (!live) {
+    return (
+      <div className="chatting_container">
+        <input
+          className="chatting_urlInput"
+          type="text"
+          placeholder="URL을 입력해주세요"
+          onChange={handleChatServerURLChange}
+          value={serverUrl}
+        />
+        <button className="chatting_connectBtn" onClick={handleSocketConnect}>
+          연결
+        </button>
+      </div>
+    );
+  }
+
   return (
     <div className="chatting_container">
-      {!live && (
-        <>
-          <>Chrome Extension Chatting Application</>
-          <input
-            className="chatting_urlInput"
-            type="text"
-            placeholder="URL을 입력해주세요"
-            onChange={setChatServerURL}
-            value={serverUrl}
-          />
-          <button className="chatting_connectBtn" onClick={onClickConnectBtn}>
-            연결
-          </button>
-        </>
-      )}
-      {live && (
-        <>
-          <div className="chatting_Room">{renderChat()}</div>
-          <input
-            className="chatting_messageInput"
-            type="text"
-            placeholder="메세지를 입력해주세요"
-            onChange={inputMessage}
-            onKeyDown={onEnter}
-            value={message}
-          />
-          <button className="chatting_sendMessage" onClick={sendMessage}>
-            전송
-          </button>
-          <br />
-          <button
-            className="chatting_disConnectBtn"
-            onClick={onClickDisconnectBtn}
-          >
-            연결 끊기
-          </button>
-        </>
-      )}
+      <div className="chatting_Room">{renderChat()}</div>
+      <input
+        className="chatting_messageInput"
+        type="text"
+        placeholder="메세지를 입력해주세요"
+        onChange={inputMessage}
+        onKeyDown={onEnter}
+        value={message}
+      />
+      <button className="chatting_sendMessage" onClick={sendMessage}>
+        전송
+      </button>
+      <br />
+      <button
+        className="chatting_disConnectBtn"
+        onClick={handleSocketDisConnect}
+      >
+        연결 끊기
+      </button>
     </div>
   );
 };
 
+interface ChatMessageProps {
+  name: string;
+  message: string;
+}
+
+function ChatMessage({ name, message }: ChatMessageProps) {
+  return (
+    <div>
+      {name}: <>{message}</>
+    </div>
+  );
+}
 export default Chatting;
