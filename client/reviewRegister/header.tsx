3bb5bb996dd0809300980b724bf2194cdd2f9f9f
sumi-0011 <selina2000@naver.com>
2023-01-06 17:01:37 +0900

    feat: review register header fix

--- client/reviewRegister/header.tsx
+++ client/reviewRegister/header.tsx
@@ -1,62 +1,32 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-import { useDispatch, useSelector } from 'react-redux';
+import Header from 'components/common/Header';
 import useGoBack from 'hooks/useGoback';
 import { dispatchPostCodeOpen } from 'store/modules/reviewRegister';
 import { RootState } from 'types';
 
-function Header() {
-  const dispatch = useDispatch();
-
-  const { POSTCODE_OPEN } = useSelector(
-    (state: RootState) => state.reviewRegister,
-  );
-
-  const postCodeOpen = () => {
-    dispatch(dispatchPostCodeOpen(!POSTCODE_OPEN));
-  };
-
+function ReviewRegisterHeader() {
+  const [state, dispatch] = useReviewRegisterRedux();
   const goBack = useGoBack();
 
+  const postCodeOpen = () => {
+    dispatch(dispatchPostCodeOpen(!state.POSTCODE_OPEN));
+  };
+
+  if (state.POSTCODE_OPEN) {
+    return <Header backAction={postCodeOpen} title="주소 찾기" />;
+  }
   return (
-    <>
-      {POSTCODE_OPEN ? (
-        <header
-          className="flex w-screen justify-between px-[18px]
-        py-4  h-24 items-center text-xl  border-b sm:w-[375px] sm:m-auto "
+    <Header
+      title="후기 작성하기"
+      rightElement={
+        <div
+          className="text-main text-[12px] Pretendard-Regular"
+          onClick={goBack}
         >
-          <div className="flex text-2xl items-center">
-            <div onClick={postCodeOpen}>
-              <FontAwesomeIcon icon="chevron-left" />
-            </div>
-            <div className="ml-4 Pretendard-SemiBold text-[17px]">
-              주소 찾기
-            </div>
-          </div>
-        </header>
-      ) : (
-        <header
-          className="flex w-screen justify-between px-[14px] 
-                py-4  h-24 items-center text-xl  border-b sm:w-[375px] sm:m-auto"
-        >
-          <div className="flex text-2xl items-center">
-            <div>
-              <FontAwesomeIcon icon="chevron-left" />
-            </div>
-            <div className="ml-4 Pretendard-SemiBold text-[17px]">
-              후기 작성하기
-            </div>
-          </div>
-          <div
-            className="text-main text-[12px] Pretendard-Regular"
-            onClick={goBack}
-          >
-            취소
-          </div>
-        </header>
-      )}
-    </>
+          취소
+        </div>
+      }
+    />
   );
 }
 
-export default Header;
+export default ReviewRegisterHeader;
--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -4,19 +4,27 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 interface HeaderProps {
   title?: string;
   rightElement?: string;
+  backAction?: () => void;
 }
 
-function Header({ title, rightElement }: HeaderProps) {
+function Header({ title, rightElement, backAction }: HeaderProps) {
   return (
-    <header className="flex w-screen justify-between px-[18px] bg-background_beige py-4  h-24 items-center text-xl  border-b min-w-[375px] sm:w-[375px]">
-      <div className="flex text-[17px] items-center items-center Pretendard-SemiBold">
-        <div>
-          <Link href="/">
-            <a>
-              <FontAwesomeIcon icon="chevron-left" />
-            </a>
-          </Link>
-        </div>
+    <header className="flex w-screen justify-between px-[18px] bg-background_beige py-4  h-24 items-center text-xl  border-b min-w-[375px] sm:w-[375px] sm:m-auto ">
+      <div className="flex text-[17px] items-center Pretendard-SemiBold">
+        {backAction ? (
+          <div onClick={backAction}>
+            <FontAwesomeIcon icon="chevron-left" />
+          </div>
+        ) : (
+          <div>
+            <Link href="/">
+              <a>
+                <FontAwesomeIcon icon="chevron-left" />
+              </a>
+            </Link>
+          </div>
+        )}
+
         <div className="text-[17px] ml-4 Pretendard-SemiBold">{title}</div>
       </div>
 
