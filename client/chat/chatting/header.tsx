838d12e8e2cfb6376e1052c47a5c43763129c8db
sumi-0011 <selina2000@naver.com>
2023-01-07 23:08:28 +0900

    feat: chatting modals useToggle 적용

--- client/chat/chatting/header.tsx
+++ client/chat/chatting/header.tsx
@@ -1,10 +1,11 @@
-import { useCallback, useState } from 'react';
+import { useCallback } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 
 import chevronLeftThickIcon from 'assets/icon/chat/chevron-left-thick.svg';
 import threeDotsVerticalIcon from 'assets/icon/chat/three-dots-vertical.svg';
 import ModalContainer from 'components/common/ModalContainer';
+import useToggle from 'hooks/useToggle';
 import MoreWindow from './modal/more';
 import ReportWindow from './modal/report';
 
@@ -12,32 +13,20 @@ function Header() {
   const router = useRouter();
 
   // 모달창 열림 상태 (더보기창, 신고사유 선택창)
-  const [moreModalOpen, setMoreModalOpen] = useState(false);
-  const [reportModalOpen, setReportModalOpen] = useState(false);
+  const [isMoreModalOpen, handleMoreModalOpenToggle] = useToggle();
+  const [isReportModalOpen, handleReportModalOpenToggle] = useToggle();
 
   /** 뒤로가기 버튼 */
   const backBtnHandler = useCallback(() => {
     router.push('/chat');
   }, [router]);
 
-  /** 더보기 버튼 - 더보기 모달창 열기 */
-  const moreBtnHandler = useCallback(() => {
-    setMoreModalOpen(true);
-  }, []);
-
-  const handleMoreModalClose = () => {
-    setMoreModalOpen(false);
-  };
-
-  const handleReportModalClose = () => {
-    setReportModalOpen(false);
-  };
-
+  /** 신고하기 버튼 - 더보기창 닫은 후 신고사유 선택창 열기 */
   const handleReportBtnClick = () => {
-    /** 신고하기 버튼 - 더보기창 닫은 후 신고사유 선택창 열기 */
-    setMoreModalOpen(false);
-    setReportModalOpen(true);
+    handleMoreModalOpenToggle();
+    handleReportModalOpenToggle();
   };
+
   return (
     <header>
       <div className="flex justify-between px-[18px] py-4 h-24 items-center text-xl border-b min-w-[375px] sm:w-[375px]">
@@ -55,7 +44,7 @@ function Header() {
         <div>
           <button
             className="flex w-[20px] h-[20px] justify-center items-center"
-            onClick={moreBtnHandler}
+            onClick={handleMoreModalOpenToggle}
           >
             <Image src={threeDotsVerticalIcon} alt="더보기 버튼" />
           </button>
@@ -63,13 +52,16 @@ function Header() {
       </div>
 
       <ModalContainer
-        isOpen={moreModalOpen}
-        onClose={handleMoreModalClose}
+        isOpen={isMoreModalOpen}
+        onClose={handleMoreModalOpenToggle}
         position="right"
       >
         <MoreWindow reportBtnHandler={handleReportBtnClick} />
       </ModalContainer>
-      <ModalContainer isOpen={reportModalOpen} onClose={handleReportModalClose}>
+      <ModalContainer
+        isOpen={isReportModalOpen}
+        onClose={handleReportModalOpenToggle}
+      >
         <ReportWindow />
       </ModalContainer>
     </header>
