353376c538abd9349a50676281529abc80c78d4c
YunSung-Choi97 <sunsu6765@gmail.com>
2023-01-10 04:32:20 +0900

    fix: fix roomApi import path error

--- saga/roomRegisterSaga.tsx
+++ saga/roomRegisterSaga.tsx
@@ -1,13 +1,13 @@
-// import { takeLatest } from 'redux-saga/effects';
+import { takeLatest } from 'redux-saga/effects';
 
-// import { roomApi } from '_api';
-// import createRequestSaga from 'utils/createRequestSaga';
+import { roomApi } from '_api/room';
+import createRequestSaga from 'utils/createRequestSaga';
 
-// const POST_ROOM = 'roomRegister/postRoom';
-// const POST_ROOM_SUCCESS = 'roomRegister/POST_SUCCESS';
+const POST_ROOM = 'roomRegister/postRoom';
+const POST_ROOM_SUCCESS = 'roomRegister/POST_SUCCESS';
 
-// const getPostSaga = createRequestSaga(POST_ROOM, roomApi.postRoom);
+const getPostSaga = createRequestSaga(POST_ROOM, roomApi.postRoom);
 
-// export function* roomRegisterSaga() {
-//   yield takeLatest(POST_ROOM, getPostSaga);
-// }
+export function* roomRegisterSaga() {
+  yield takeLatest(POST_ROOM, getPostSaga);
+}
