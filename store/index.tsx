27a4ad5953a405abcf9eeb8a4c3f2848e8a9708f
YunSung-Choi97 <sunsu6765@gmail.com>
2023-01-11 00:58:37 +0900

    update: add redux middleware logger

--- store/index.tsx
+++ store/index.tsx
@@ -1,12 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { createWrapper } from 'next-redux-wrapper';
+import logger from 'redux-logger';
 
 import rootReducer from './modules';
 
-const store = configureStore({
-  reducer: rootReducer,
-  devTools: process.env.NEXTNODE_ENV !== 'production',
-});
+const isNotProductionEnv = process.env.NODE_ENV !== 'production';
+
+const store = isNotProductionEnv
+  ? configureStore({
+      reducer: rootReducer,
+      middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(logger),
+      devTools: isNotProductionEnv,
+    })
+  : configureStore({
+      reducer: rootReducer,
+      devTools: isNotProductionEnv,
+    });
 
 const makeStore = () => {
   return store;
@@ -15,5 +25,5 @@ const makeStore = () => {
 export type AppDispatch = typeof store.dispatch;
 
 export const wrapper = createWrapper(makeStore, {
-  debug: process.env.NODE_ENV !== 'production',
+  debug: isNotProductionEnv,
 });
